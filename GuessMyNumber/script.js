70abd8c64cfa6e00eb9b3e8387430ef9e8411f80
Shea Develops <sheadevelops@gmail.com>
2021-09-06 22:17:32 -1000

    Add click event for check button to check for empty value

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,2 +1,10 @@
 'use strict';
 
+// GET CHECK CLASS ELEMENT ADD CLICK EVENT
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
