0f9bb10bcaa1613890eef569367c5dc50f421375
Shea Develops <sheadevelops@gmail.com>
2021-09-07 05:30:50 -1000

    Add win condition

--- OneDice/script.js
+++ OneDice/script.js
@@ -4,6 +4,7 @@
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 // SELECT ELEMENTS
 const player0El = document.querySelector('.player--0');
@@ -22,9 +23,20 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+// SWITCH PLAYER
+const switchPlayer = function () {
+  // SET CURRENT PLAYER SCORE TO 0 AND SIWTCH PLAYERS
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // ROLLING DICE
 btnRoll.addEventListener('click', function () {
-
+  if (playing) {
     // GENERATE RANDOM DICE ROLL
     const dice = Math.trunc(Math.random() * 6) + 1;
 
@@ -33,21 +45,37 @@ btnRoll.addEventListener('click', function () {
     diceEl.src = `images/dice-${dice}.png`;
 
     // CHECK FOR ROLL OF 1 / SWITPLAYER
-    if(dice !== 1) {
-
-        // ADD DICE TO CURRENT SCORE
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    if (dice !== 1) {
+      // ADD DICE TO CURRENT SCORE
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
     } else {
-
-        // SET CURRENT PLAYER SCORE TO 0 AND SIWTCH PLAYERS
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-
-        // RESET BACK TO ZERO
-        currentScore = 0;
-
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+      switchPlayer();
     }
+  }
+});
+
+// HOLD PLAYER POINTS
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // ADD CURRENT SCORE TO ACTIVE PLAYER
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // CHECK IF PLAYER SCORE IS >= 100
+    if (scores[activePlayer] >= 10) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
 });
