89485d7a616e7965fe3018e5e5bb9ad489ce4d72
Shea Develops <sheadevelops@gmail.com>
2021-09-07 02:58:08 -1000

    Add starting conditons

--- OneDice/script.js
+++ OneDice/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// SELECT OUR SCORE ELEMENTS
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// STARTING CONDITIONS
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
