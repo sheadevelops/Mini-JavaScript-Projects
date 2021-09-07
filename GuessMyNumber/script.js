3c3eb32a65b6b5e910b10b60d18bdfc699a9d906
Shea Develops <sheadevelops@gmail.com>
2021-09-06 23:20:05 -1000

    Add highscore functionality

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -3,6 +3,10 @@
 // SCORE
 let score = 20;
 
+// HIGHSCORE
+let highscore = 0;
+document.querySelector('.highscore').textContent = highscore;
+
 // CREATE OUR SUPER SECRET NUMBER
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
@@ -21,13 +25,19 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
+    // UPDATE HIGHSCORE
+    if(score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+    }
+
     // PLAYER GUESSES HIGH
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '☝ Too High';
       score--;
       document.querySelector('.score').textContent = score;
-      
+
       // PLAYER LOST
     } else {
       document.querySelector('.message').textContent = '😢 You lost the game!';
@@ -50,23 +60,24 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 // RESET THE GAME
-document.querySelector('.again').addEventListener('click', function() {
+document.querySelector('.again').addEventListener('click', function () {
+  // SET SCORE BACK TO 20
+  score = 20;
+  document.querySelector('.score').textContent = score;
 
-    // SET SCORE BACK TO 20
-    score = 20;
-    document.querySelector('.score').textContent = score;
+  // RESET MESSAGE
+  document.querySelector('.message').textContent = 'Start guessing...';
 
-    // RESET MESSAGE
-    document.querySelector('.message').textContent = 'Start guessing...';
+  // CLEAR PLAYER GUESS
+  document.querySelector('.guess').value = '';
 
-    // CLEAR PLAYER GUESS
-    document.querySelector('.guess').value = '';
+  // NEW SECRET NUMBER
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-    // NEW SECRET NUMBER
-    secretNumber = Math.trunc(Math.random() * 20) + 1;
+  // REVERT WINNING CSS CHANGES
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 
-    // REVERT WINNING CSS CHANGES
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
-    document.querySelector('.number').textContent = '?';
+  // HIDE NEW SECRET NUMBER
+  document.querySelector('.number').textContent = '?';
 });
