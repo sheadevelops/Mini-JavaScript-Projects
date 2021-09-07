ce42a64a6465cc3ea562c8f4eaebf6433ff56357
Shea Develops <sheadevelops@gmail.com>
2021-09-07 03:16:17 -1000

    Add dice rolling functionality

--- OneDice/script.js
+++ OneDice/script.js
@@ -1,11 +1,42 @@
 'use strict';
 
-// SELECT OUR SCORE ELEMENTS
+// SCORES
+let currentScore = 0;
+let player1Score = 0;
+let player2Score = 0;
+
+// SELECT ELEMENTS
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // STARTING CONDITIONS
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// ROLLING DICE
+btnRoll.addEventListener('click', function () {
+
+    // GENERATE RANDOM DICE ROLL
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // DISPLACE DICE
+    diceEl.classList.remove('hidden');
+    diceEl.src = `images/dice-${dice}.png`;
+
+    // CHECK FOR ROLL OF 1 / SWITPLAYER
+    if(dice !== 1) {
+        
+        // ADD DICE TO CURRENT SCORE
+        currentScore += dice;
+        current0El.textContent = currentScore; // CHANGE LATER
+    } else {
+        // SWITCH TO NEXT PLAYER
+    }
+});
