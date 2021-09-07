64e880252b1b6df23ad4aed5e8db81bec4f08eeb
Shea Develops <sheadevelops@gmail.com>
2021-09-07 02:31:06 -1000

    Add esc key functionality for modal

--- ModalWindow/script.js
+++ ModalWindow/script.js
@@ -8,22 +8,27 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 // OPEN THE MODAL
 const openModal = function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 // CLOSE THE MODAL
 const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-}
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 // WALKTHROUGH BUTTONS FOR THE MODAL TO ADD A CLICK LISTENER TO OPEN THE MODAL
-for(let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', openModal);
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 
 // CLOSE THE MODAL
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('.hidden')) {
+    closeModal();
+  }
+});
