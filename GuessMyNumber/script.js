c026465a1a0b1bc34196766f398474d5a76bf788
Shea Develops <sheadevelops@gmail.com>
2021-09-06 22:45:18 -1000

    Add game logic for checking if guessed value is above or below secretNumber

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,10 +1,37 @@
 'use strict';
 
+// SCORE
+let score = 20;
+
+// CREATE OUR SUPER SECRET NUMBER
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 // GET CHECK CLASS ELEMENT ADD CLICK EVENT
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '☝ Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😢 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '👇 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😢 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
