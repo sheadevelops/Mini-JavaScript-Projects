949cd4570bf057b1c27b4408b1d6366338dc0584
Shea Develops <sheadevelops@gmail.com>
2021-09-23 15:58:03 -1000

    Add visual page updates, localStorage write/read

---
+++ SeatBooking/scripts.js
@@ -0,0 +1,71 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+populateUI();
+
+let ticketPrice = +movieSelect.value;
+
+// SAVE MOVIE INDEX AND PRICE
+function setMovieData(movieIndex, moviePrice) {
+  // localStorage is saved on the client and can be accessed via the developer options Application tab
+  // if a user refreshes the page the localStorage can be recovered and used to populate the page
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// GRAB LOCALSTORAGE DATA AND INSERT INTO PAGE 
+// localStorage data can persist indefinitely because there is no expiration date, 
+// however it can be cleared by other factors, sometimes on accident.
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if(selectedMovieIndex !== null) {
+      movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+// MOVIE SELECTOR ON CHANGE EVENT
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+// SEAT CLICKER VERIFICATION
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
+
+// SET TOTAL AND COUNT
+updateSelectedCount();
