70f51152f798c4e0b5186d01be8639ea672f7fa0
Shea Develops <sheadevelops@gmail.com>
2021-09-26 22:05:44 -1000

    Display created users on page, format currency, Add user functionality

--- ArrayPractice/script.js
+++ ArrayPractice/script.js
@@ -32,4 +32,27 @@ async function getRandomUser() {
 // Add new data obj to data arr
 function addData(obj) {
     data.push(obj);
+
+    updateDOM();
 }
+
+// Update the DOM | If no data is provided we use the data[]
+function updateDOM(providedData = data) {
+    // Clearing main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element);
+    });
+}
+
+// Format money
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');  // 12,345.67
+}
+
+// Event Listeners
+addUserBtn.addEventListener('click', getRandomUser);
